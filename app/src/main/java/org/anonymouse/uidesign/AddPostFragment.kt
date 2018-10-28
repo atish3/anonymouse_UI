f9d7b3406d60b0094f10e511193878130292d8b4
chshibo <chshibo@umich.edu>
2018-10-28 14:27:27 -0400

    some fixes

--- app/src/main/java/org/anonymouse/uidesign/AddPostFragment.kt
+++ app/src/main/java/org/anonymouse/uidesign/AddPostFragment.kt
@@ -1,14 +1,20 @@
 package org.anonymouse.uidesign
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_post_list.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_add_post.view.*
 import kotlinx.android.synthetic.main.fragment_post_list.*
+import java.util.jar.Manifest
 
 class AddPostFragment : Fragment() {
 
@@ -17,8 +23,16 @@ class AddPostFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
+        val IMAGEREQUESTCODE = 8242008
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_post, container, false)
+        val view = inflater.inflate(R.layout.fragment_add_post, container, false)
+        view.btn_add_photo.setOnClickListener{
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            startActivityForResult(intent, IMAGEREQUESTCODE)
+        }
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
